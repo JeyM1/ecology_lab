32b5e3527d2320bf3cbbe3e77f0c86a8bdee24b4
JeyM1 <denafen2000@gmail.com>
2020-05-02 19:56:30 +0300

    +sulfur dioxide

--- scripts/main_calculate.js
+++ scripts/main_calculate.js
@@ -3,6 +3,25 @@ function getInputValue(){
     var inputVal = document.form_name.input.value;
     
     // Displaying the value
-    document.getElementById("out").innerHTML = "Hello " + inputVal;
+
+    // test for SO2
+    Bi = 1096363;
+    Qri = 20.47;
+    Sr = 2.85;
+    n1 = 0.05;
+    n2 = 0;
+    Beta = 1;
+
+    document.getElementById("out").innerHTML = "E =  " + getEmission(getKSulfurDioxideSO2(Qri, Sr, n1, n2, Beta), Qri, Bi) + "<br>k = " + getKSulfurDioxideSO2(Qri, Sr, n1, n2, Beta);
     document.getElementById("out_div").hidden = false;
+}
+
+
+function getKSulfurDioxideSO2(Qri, Sr, n1, n2, Beta) {
+    k = (Math.pow(10, 6) / Qri) * (0.02 * Sr) * (1 - n1) * (1 - (n2 * Beta));
+    return k;
+}
+
+function getEmission(c, Qri, B) {
+    return Math.pow(10, -6) * c * Qri * B;
 }
