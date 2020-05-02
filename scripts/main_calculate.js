d86ebcb1dfbf9b858af107305b23f691f711fe48
JeyM1 <denafen2000@gmail.com>
2020-05-02 21:24:12 +0300

    +SuspendedSolidParticles

--- scripts/main_calculate.js
+++ scripts/main_calculate.js
@@ -3,9 +3,10 @@ function getInputValue(){
     var inputVal = document.form_name.input.value;
     
     // Displaying the value
-
-    // test for SO2
     Bi = 1096363;
+    Qri = 20.47;
+    // test for SO2
+    /*Bi = 1096363;
     Qri = 20.47;
     Sr = 2.85;
     n1 = 0.05;
@@ -13,15 +14,21 @@ function getInputValue(){
     Beta = 1;
 
     document.getElementById("out").innerHTML = "E =  " + getEmission(getKSulfurDioxideSO2(Qri, Sr, n1, n2, Beta), Qri, Bi) + "<br>k = " + getKSulfurDioxideSO2(Qri, Sr, n1, n2, Beta);
+    document.getElementById("out_div").hidden = false;*/
+
+    document.getElementById("out").innerHTML = "E =  " + getEmission(getKSuspendedSolidParticles(20.47, 0.8, 25.20, 1.5, 0.985, 0), Qri, Bi) +
+                                                "<br>k = " + getKSuspendedSolidParticles(20.47, 0.8, 25.20, 1.5, 0.985, 0);
     document.getElementById("out_div").hidden = false;
-}
-
-
-function getKSulfurDioxideSO2(Qri, Sr, n1, n2, Beta) {
-    k = (Math.pow(10, 6) / Qri) * (0.02 * Sr) * (1 - n1) * (1 - (n2 * Beta));
-    return k;
 }
 
 function getEmission(c, Qri, B) {
     return Math.pow(10, -6) * c * Qri * B;
+}
+
+function getKSulfurDioxideSO2(Qri, Sr, n1, n2, Beta) {
+    return (Math.pow(10, 6) / Qri) * (0.02 * Sr) * (1 - n1) * (1 - (n2 * Beta));
+}
+
+function getKSuspendedSolidParticles(Qri, a, Ar, G, n, k_emission) {
+    return (Math.pow(10, 6) / Qri) * a * (Ar / (100 - G)) * (1 - n) + k_emission;
 }
